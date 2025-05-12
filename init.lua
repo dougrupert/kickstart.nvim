c53c19f637938d333e20fe7f67d4338d76ea9d96
dougrupert <77610361+dougrupert@users.noreply.github.com>
2025-05-12 16:06:38 +0200

    load vimrc.vim

--- init.lua
+++ init.lua
@@ -1012,5 +1012,9 @@ require('lazy').setup({
   },
 })
 
+-- Vimscript config
+local vimrc = vim.fn.stdpath 'config' .. '/vimrc.vim'
+vim.cmd.source(vimrc)
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
