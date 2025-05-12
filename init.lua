c1be1a95ae5b4ef087b6a8961de98a050ce03d12
dougrupert <77610361+dougrupert@users.noreply.github.com>
2025-05-12 17:17:02 +0200

    enable system clipboard for wsl2

--- init.lua
+++ init.lua
@@ -114,9 +114,9 @@ vim.o.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
+-- vim.schedule(function()
+--   vim.o.clipboard = 'unnamedplus'
+-- end)
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -1015,6 +1015,18 @@ require('lazy').setup({
 -- Vimscript config
 local vimrc = vim.fn.stdpath 'config' .. '/vimrc.vim'
 vim.cmd.source(vimrc)
+
+-- sync with system clipboard on focus
+vim.api.nvim_create_autocmd({ 'FocusGained' }, {
+  pattern = { '*' },
+  command = [[call setreg("@", getreg("+"))]],
+})
+
+-- sync with system clipboard on focus
+vim.api.nvim_create_autocmd({ 'FocusLost' }, {
+  pattern = { '*' },
+  command = [[call setreg("+", getreg("@"))]],
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
